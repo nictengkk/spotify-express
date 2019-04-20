9d9d99bd68ef81c1639775fa8409a257ed05a212
nictengkk <nictengkk@gmail.com>
2019-04-21 01:44:59 +0800

    import Oauth from spotify example, update /login route

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const index = require("./routes/index");
+const cookieParser = require("cookie-parser");
 
+const isDev = process.env.NODE_ENV !== "production";
+
+const whitelist = [];
+if (isDev) {
+  whitelist.push("http://localhost:3000");
+}
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", index);
 
--- routes/index/index.js
+++ routes/index/index.js
@@ -1,7 +1,133 @@
 const express = require("express");
+const querystring = require("querystring");
+const generateRandomString = require("../../services/generateRandomString");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+const redirect_uri = "http://localhost:8888/callback";
+const stateKey = "spotify_auth_state";
+
+router.route("/login").get((req, res) => {
+  let state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  const scope = "user-read-playback-state user-read-private user-read-email";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      })
+  );
+});
+
+router.route("/callback").get((req, res) => {
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  let code = req.query.code || null;
+  let state = req.query.state || null;
+  let storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch"
+        })
+    );
+  } else {
+    res.clearCookie(stateKey);
+    let authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code"
+      },
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer.from(
+            process.env.SPOTIFY_CLIENT_ID +
+              ":" +
+              process.env.SPOTIFY_CLIENT_SECRET
+          ).toString("base64")
+      },
+      json: true
+    };
+
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        let access_token = body.access_token;
+        let refresh_token = body.refresh_token;
+
+        let options = {
+          url: "https://api.spotify.com/v1/me",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, (error, response, body) => {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect(
+          "http://localhost:3000/#" +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token
+            })
+        );
+      } else {
+        res.redirect(
+          "/#" +
+            querystring.stringify({
+              error: "invalid_token"
+            })
+        );
+      }
+    });
+  }
+});
+
+router.route("/refresh_token").get((req, res) => {
+  // requesting access token from refresh token
+  let refresh_token = req.query.refresh_token;
+  let authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ).toString("base64")
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let access_token = body.access_token;
+      res.send({
+        access_token: access_token
+      });
+    }
+  });
+});
+
+router.route("/").get((req, res) => {
   res.send("Welcome, please login");
 });
 
